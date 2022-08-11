192d9a80aaa33055f01082a7a3240d40bd0bdcc7
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-08-11 18:32:19 +0530

    Update zombie.py

--- Flare_Robot/modules/zombie.py
+++ Flare_Robot/modules/zombie.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from Akenobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from Flare_Robot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
